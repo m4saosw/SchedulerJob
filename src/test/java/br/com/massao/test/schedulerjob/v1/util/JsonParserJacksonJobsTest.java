5ad8a8948b072606639d3c1c8f9c80b3a7d4bd60
Massao <lmassao@yahoo.com.br>
2020-05-21 00:15:49 -0300

    v1 - SchedulerJob - ajustes nas classes modelo de entrada: deserialização json; tipo dados - classes teste do conversor jackson - cenarios erro e sucesso

--- src/test/java/br/com/massao/test/schedulerjob/v1/util/JsonParserJacksonJobsTest.java
+++ src/test/java/br/com/massao/test/schedulerjob/v1/util/JsonParserJacksonJobsTest.java
@@ -1,15 +1,22 @@
 package br.com.massao.test.schedulerjob.v1.util;
 
+import br.com.massao.test.schedulerjob.v1.model.input.Job;
 import br.com.massao.test.schedulerjob.v1.model.input.Jobs;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
 
 public class JsonParserJacksonJobsTest {
 
     @Test(expected = Exception.class)
     public void recusarJSonNulo() throws JsonProcessingException {
         String jSon = null;
-        Jobs jobs = JsonParserJacksonJobs.toClass(jSon);
+        JsonParserJacksonJobs.toClass(jSon);
     }
 
 
@@ -27,24 +34,63 @@ public class JsonParserJacksonJobsTest {
 
     @Test(expected = Exception.class)
     public void recusarJSonConteudoInvalido_TipoDadosInvalidoNoCampo() throws JsonProcessingException {
-        String jSon = "";
+        String jSon = "[\n" +
+                "  {\n" +
+                "    \"ID\": 1,\n" +
+                "    \"Descrição\": \"Importação de arquivos de fundos\",\n" +
+                "    \"Data Máxima de conclusão\": \"2019-11-10 12:00:00\",\n" +
+                "    \"Tempo estimado\": Este e um conteudo invalido no campo tempo\n" +
+                "  }\n" +
+                "]";
         JsonParserJacksonJobs.toClass(jSon);
     }
 
 
     @Test
-    public void aceitarJSonValido_UmJob() throws JsonProcessingException {
-        /*String jSon = "";
-        Jobs jobs = JsonParserJacksonJobs.toClass(jSon);*/
-        // todo - pendente
+    public void aceitarJSonValido_ContendoUmJob() throws JsonProcessingException {
+        String jSon = "[\n" +
+                "  {\n" +
+                "    \"ID\": 1,\n" +
+                "    \"Descrição\": \"Importação de arquivos de fundos\",\n" +
+                "    \"Data Máxima de conclusão\": \"2019-11-10 12:00:00\",\n" +
+                "    \"Tempo estimado\": 2\n" +
+                "  }\n" +
+                "]";
+        Jobs jobs = JsonParserJacksonJobs.toClass(jSon);
+        Job job = jobs.getJobs().stream().findFirst().get();
+
+        assertEquals(1, job.getId());
+        assertEquals("2019-11-10 12:00:00", job.getDeadline().format(Job.FORMATTER));
+        assertEquals(2f, job.getEstimatedTime(), 0.0f);
     }
 
     @Test
-    public void aceitarJSonValido_VariosJob() throws JsonProcessingException {
-        /*String jSon = "";
-        Jobs jobs = JsonParserJacksonJobs.toClass(jSon);*/
-        // todo - pendente
+    public void aceitarJSonValido_Contendo3Job() throws JsonProcessingException {
+        String jSon = "[\n" +
+                "  {\n" +
+                "    \"ID\": 1,\n" +
+                "    \"Descrição\": \"Importação de arquivos de fundos\",\n" +
+                "    \"Data Máxima de conclusão\": \"2019-11-10 12:00:00\",\n" +
+                "    \"Tempo estimado\": 2\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"ID\": 2,\n" +
+                "    \"Descrição\": \"Importação de dados da Base Legada\",\n" +
+                "    \"Data Máxima de conclusão\": \"2019-11-11 12:00:00\",\n" +
+                "    \"Tempo estimado\": 4\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"ID\": 3,\n" +
+                "    \"Descrição\": \"Importação de dados de integração\",\n" +
+                "    \"Data Máxima de conclusão\": \"2019-11-11 08:00:00\",\n" +
+                "    \"Tempo estimado\": 6\n" +
+                "  }\n" +
+                "]";
+        Jobs jobs = JsonParserJacksonJobs.toClass(jSon);
+        List<Job> jobsList = jobs.getJobs().stream().collect(Collectors.toList());
+
+        assertEquals(1, jobsList.get(0).getId());
+        assertEquals(2, jobsList.get(1).getId());
+        assertEquals(3, jobsList.get(2).getId());
     }
-
-
 }
